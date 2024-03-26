f49d6743c21fbd3c43ae292b60eb33e5a311fc74
Amer-Ak <ameralkadri201@gmail.com>
2024-03-26 09:11:09 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace DraggingPictures_22SD_Humam_barjak
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbxOptions = new System.Windows.Forms.GroupBox();
+            this.pcbsisssor = new System.Windows.Forms.PictureBox();
+            this.pcbpapier = new System.Windows.Forms.PictureBox();
+            this.pcbrock = new System.Windows.Forms.PictureBox();
+            this.gbxBot = new System.Windows.Forms.GroupBox();
+            this.pcbBotThree = new System.Windows.Forms.PictureBox();
+            this.pcbBotTwo = new System.Windows.Forms.PictureBox();
+            this.pcbBotOne = new System.Windows.Forms.PictureBox();
+            this.gbxPlayer = new System.Windows.Forms.GroupBox();
+            this.picBoxPlayer3 = new System.Windows.Forms.PictureBox();
+            this.picBoxPlayer2 = new System.Windows.Forms.PictureBox();
+            this.picBoxPlayer1 = new System.Windows.Forms.PictureBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.gbxOptions.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbsisssor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbpapier)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbrock)).BeginInit();
+            this.gbxBot.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbBotThree)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbBotTwo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbBotOne)).BeginInit();
+            this.gbxPlayer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxPlayer3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxPlayer2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbxOptions
+            // 
+            this.gbxOptions.Controls.Add(this.pcbsisssor);
+            this.gbxOptions.Controls.Add(this.pcbpapier);
+            this.gbxOptions.Controls.Add(this.pcbrock);
+            this.gbxOptions.Location = new System.Drawing.Point(16, 15);
+            this.gbxOptions.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gbxOptions.Name = "gbxOptions";
+            this.gbxOptions.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gbxOptions.Size = new System.Drawing.Size(271, 523);
+            this.gbxOptions.TabIndex = 0;
+            this.gbxOptions.TabStop = false;
+            this.gbxOptions.Text = "Options";
+            // 
+            // pcbsisssor
+            // 
+            this.pcbsisssor.Image = global::DraggingPictures_22SD_Humam_barjak.Properties.Resources.sisssor;
+            this.pcbsisssor.Location = new System.Drawing.Point(31, 361);
+            this.pcbsisssor.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pcbsisssor.Name = "pcbsisssor";
+            this.pcbsisssor.Size = new System.Drawing.Size(208, 132);
+            this.pcbsisssor.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcbsisssor.TabIndex = 2;
+            this.pcbsisssor.TabStop = false;
+            this.pcbsisssor.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pcboptions_MouseDown);
+            // 
+            // pcbpapier
+            // 
+            this.pcbpapier.Image = global::DraggingPictures_22SD_Humam_barjak.Properties.Resources.papier;
+            this.pcbpapier.Location = new System.Drawing.Point(31, 196);
+            this.pcbpapier.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pcbpapier.Name = "pcbpapier";
+            this.pcbpapier.Size = new System.Drawing.Size(208, 132);
+            this.pcbpapier.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcbpapier.TabIndex = 1;
+            this.pcbpapier.TabStop = false;
+            this.pcbpapier.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pcboptions_MouseDown);
+            // 
+            // pcbrock
+            // 
+            this.pcbrock.Image = global::DraggingPictures_22SD_Humam_barjak.Properties.Resources.rock_copy;
+            this.pcbrock.Location = new System.Drawing.Point(29, 38);
+            this.pcbrock.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pcbrock.Name = "pcbrock";
+            this.pcbrock.Size = new System.Drawing.Size(208, 132);
+            this.pcbrock.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcbrock.TabIndex = 0;
+            this.pcbrock.TabStop = false;
+            this.pcbrock.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pcboptions_MouseDown);
+            // 
+            // gbxBot
+            // 
+            this.gbxBot.Controls.Add(this.pcbBotThree);
+            this.gbxBot.Controls.Add(this.pcbBotTwo);
+            this.gbxBot.Controls.Add(this.pcbBotOne);
+            this.gbxBot.Location = new System.Drawing.Point(312, 27);
+            this.gbxBot.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gbxBot.Name = "gbxBot";
+            this.gbxBot.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gbxBot.Size = new System.Drawing.Size(780, 223);
+            this.gbxBot.TabIndex = 1;
+            this.gbxBot.TabStop = false;
+            this.gbxBot.Text = "Bot";
+            // 
+            // pcbBotThree
+            // 
+            this.pcbBotThree.Location = new System.Drawing.Point(548, 39);
+            this.pcbBotThree.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pcbBotThree.Name = "pcbBotThree";
+            this.pcbBotThree.Size = new System.Drawing.Size(188, 144);
+            this.pcbBotThree.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcbBotThree.TabIndex = 2;
+            this.pcbBotThree.TabStop = false;
+            // 
+            // pcbBotTwo
+            // 
+            this.pcbBotTwo.Location = new System.Drawing.Point(296, 39);
+            this.pcbBotTwo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pcbBotTwo.Name = "pcbBotTwo";
+            this.pcbBotTwo.Size = new System.Drawing.Size(188, 144);
+            this.pcbBotTwo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcbBotTwo.TabIndex = 1;
+            this.pcbBotTwo.TabStop = false;
+            // 
+            // pcbBotOne
+            // 
+            this.pcbBotOne.Location = new System.Drawing.Point(36, 43);
+            this.pcbBotOne.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pcbBotOne.Name = "pcbBotOne";
+            this.pcbBotOne.Size = new System.Drawing.Size(188, 144);
+            this.pcbBotOne.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcbBotOne.TabIndex = 0;
+            this.pcbBotOne.TabStop = false;
+            // 
+            // gbxPlayer
+            // 
+            this.gbxPlayer.Controls.Add(this.picBoxPlayer3);
+            this.gbxPlayer.Controls.Add(this.picBoxPlayer2);
+            this.gbxPlayer.Controls.Add(this.picBoxPlayer1);
+            this.gbxPlayer.Location = new System.Drawing.Point(312, 284);
+            this.gbxPlayer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gbxPlayer.Name = "gbxPlayer";
+            this.gbxPlayer.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gbxPlayer.Size = new System.Drawing.Size(780, 223);
+            this.gbxPlayer.TabIndex = 2;
+            this.gbxPlayer.TabStop = false;
+            this.gbxPlayer.Text = "Player";
+            // 
+            // picBoxPlayer3
+            // 
+            this.picBoxPlayer3.Location = new System.Drawing.Point(548, 46);
+            this.picBoxPlayer3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.picBoxPlayer3.Name = "picBoxPlayer3";
+            this.picBoxPlayer3.Size = new System.Drawing.Size(188, 144);
+            this.picBoxPlayer3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBoxPlayer3.TabIndex = 3;
+            this.picBoxPlayer3.TabStop = false;
+            this.picBoxPlayer3.DragDrop += new System.Windows.Forms.DragEventHandler(this.pcbAllPlayer_DragDrop);
+            this.picBoxPlayer3.DragOver += new System.Windows.Forms.DragEventHandler(this.pcbAllPlayer_DragOver);
+            // 
+            // picBoxPlayer2
+            // 
+            this.picBoxPlayer2.Location = new System.Drawing.Point(296, 46);
+            this.picBoxPlayer2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.picBoxPlayer2.Name = "picBoxPlayer2";
+            this.picBoxPlayer2.Size = new System.Drawing.Size(188, 144);
+            this.picBoxPlayer2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBoxPlayer2.TabIndex = 2;
+            this.picBoxPlayer2.TabStop = false;
+            this.picBoxPlayer2.DragDrop += new System.Windows.Forms.DragEventHandler(this.pcbAllPlayer_DragDrop);
+            this.picBoxPlayer2.DragOver += new System.Windows.Forms.DragEventHandler(this.pcbAllPlayer_DragOver);
+            // 
+            // picBoxPlayer1
+            // 
+            this.picBoxPlayer1.Location = new System.Drawing.Point(36, 46);
+            this.picBoxPlayer1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.picBoxPlayer1.Name = "picBoxPlayer1";
+            this.picBoxPlayer1.Size = new System.Drawing.Size(188, 144);
+            this.picBoxPlayer1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBoxPlayer1.TabIndex = 1;
+            this.picBoxPlayer1.TabStop = false;
+            this.picBoxPlayer1.DragDrop += new System.Windows.Forms.DragEventHandler(this.pcbAllPlayer_DragDrop);
+            this.picBoxPlayer1.DragOver += new System.Windows.Forms.DragEventHandler(this.pcbAllPlayer_DragOver);
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(825, 523);
+            this.btnCheck.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(129, 43);
+            this.btnCheck.TabIndex = 3;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(963, 523);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(129, 43);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1113, 581);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.gbxPlayer);
+            this.Controls.Add(this.gbxBot);
+            this.Controls.Add(this.gbxOptions);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gbxOptions.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pcbsisssor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbpapier)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbrock)).EndInit();
+            this.gbxBot.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pcbBotThree)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbBotTwo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbBotOne)).EndInit();
+            this.gbxPlayer.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxPlayer3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxPlayer2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbxOptions;
+        private System.Windows.Forms.PictureBox pcbsisssor;
+        private System.Windows.Forms.PictureBox pcbpapier;
+        private System.Windows.Forms.PictureBox pcbrock;
+        private System.Windows.Forms.GroupBox gbxBot;
+        private System.Windows.Forms.GroupBox gbxPlayer;
+        private System.Windows.Forms.PictureBox pcbBotThree;
+        private System.Windows.Forms.PictureBox pcbBotTwo;
+        private System.Windows.Forms.PictureBox pcbBotOne;
+        private System.Windows.Forms.PictureBox picBoxPlayer3;
+        private System.Windows.Forms.PictureBox picBoxPlayer2;
+        private System.Windows.Forms.PictureBox picBoxPlayer1;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
